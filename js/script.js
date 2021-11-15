984e25322344042267ae9de8b52c134cdbd82936
MolodovA <80354289+MolodovA@users.noreply.github.com>
2021-11-15 21:33:50 +0300

    added logo

--- js/script.js
+++ js/script.js
@@ -5,6 +5,8 @@
 function square(a) {
    return a * a;
 }
-
+//==============================================================
 //Make a simple function called greet that returns the most-famous "hello world!".
 let greet = () => 'hello world!'
+//==============================================================
+
