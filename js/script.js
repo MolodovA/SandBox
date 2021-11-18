c34e3277176b966500c90f13855f9b720547dc7e
MolodovA <80354289+MolodovA@users.noreply.github.com>
2021-11-18 10:10:37 +0300

    solution simple task

--- js/script.js
+++ js/script.js
@@ -16,3 +16,8 @@ let multiply = (a, b) => a * b;
 //==============================================================
 // Implement a function which convert the given boolean value into its string representation
 let booleanToString = (b) => b ? 'true' : 'false'
+
+//==============================================================
+// Build a function that takes in two arguments (salary, bonus). Salary will be an integer, and bonus a boolean.
+// If bonus is true, the salary should be multiplied by 10. If bonus is false, the fatcat did not make enough money and must receive only his stated salary.
+const bonusTime = (salary, bonus) => bonus ? '£' + salary * 10 : '£' + salary
