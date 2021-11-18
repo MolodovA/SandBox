682273ba9c826b3ce5aa27b343fb3306864aad23
MolodovA <80354289+MolodovA@users.noreply.github.com>
2021-11-18 12:22:56 +0300

    solution simple task

--- js/script.js
+++ js/script.js
@@ -5,23 +5,23 @@
 function square(a) {
    return a * a;
 }
-//==============================================================
+//===========================================================================
 //Make a simple function called greet that returns the most-famous "hello world!".
 let greet = () => 'hello world!'
-//==============================================================
+//============================================================================
 
 // Implement a function which multiplies two numbers.
 let multiply = (a, b) => a * b;
 
-//==============================================================
+//=============================================================================
 // Implement a function which convert the given boolean value into its string representation
 let booleanToString = (b) => b ? 'true' : 'false'
 
-//==============================================================
+//================================================================================
 // Build a function that takes in two arguments (salary, bonus). Salary will be an integer, and bonus a boolean.
 // If bonus is true, the salary should be multiplied by 10. If bonus is false, the fatcat did not make enough money and must receive only his stated salary.
 const bonusTime = (salary, bonus) => bonus ? '£' + salary * 10 : '£' + salary
-
+//===================================================================================
 // Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
 const moveZeros = function (arr) {
    let zero = [];
@@ -38,3 +38,9 @@ const moveZeros = function (arr) {
    }
    return joint = remain.concat(zero)
 }
+//==================================================================================
+// Use destructuring assignment to swap the values of a and b so that a receives the value stored in b, and b receives the value stored in a.Use destructuring assignment to swap the values of a and b so that a receives the value stored in b, and b receives the value stored in a.
+let [a, b] = [6, 8]
+
+[a, b] = [b, a]
+
