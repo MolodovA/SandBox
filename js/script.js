df7330223f6ee8810eb1a151f0f7d408166cc926
MolodovA <80354289+MolodovA@users.noreply.github.com>
2021-11-19 21:36:13 +0300

    solution task

--- js/script.js
+++ js/script.js
@@ -55,3 +55,7 @@ function countBits(num) {
    }
    return count;
 }
+
+
+// The cockroach is one of the fastest insects. Write a function which takes its speed in km per hour and returns it in cm per second, rounded down to the integer (= floored).
+const cockroachSpeed = (s) => Math.round(s * (100 / 3.6))
