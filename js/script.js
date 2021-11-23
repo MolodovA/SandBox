854bce8aed4d7d2f2fd4034bf8ba6d73ec424f5d
MolodovA <80354289+MolodovA@users.noreply.github.com>
2021-11-23 23:35:46 +0300

    solution task

--- js/script.js
+++ js/script.js
@@ -122,3 +122,12 @@ class Kata {
       return length * width * height
    }
 }
+
+// Write a function that returns the total surface area and volume of a box as an array: [area, volume]
+function getSize(w, h, d) {
+   let area = 2 * (d * w) + 2 * (d * h) + 2 * (h * w)
+   let volume = w * h * d;
+   let arr = [];
+   arr.push(area, volume);
+   return arr;
+}
