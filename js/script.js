5c9c9fd11eebfacb9276099f92385f907924bad6
MolodovA <80354289+MolodovA@users.noreply.github.com>
2021-12-12 22:46:01 +0300

    solution task

--- js/script.js
+++ js/script.js
@@ -171,3 +171,9 @@ function validParentheses(parens) {
 }
 //Very simple, given an integer or a floating-point number, find its opposite.
 const opposite = (n) => n * -1
+
+
+
+//Your classmates asked you to copy some paperwork for them.You know that there are 'n' classmates and the paperwork has 'm' pages.
+//Your task is to calculate how many blank pages do you need.If n < 0 or m < 0 return 0.
+const paperwork = (n, m) => n > 0 && m > 0 ? n * m : 0
