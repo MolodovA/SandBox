c3d4e354724077982fa620ce36952b233f9cb45c
MolodovA <80354289+MolodovA@users.noreply.github.com>
2021-12-16 20:23:16 +0300

    solution task

--- js/script.js
+++ js/script.js
@@ -177,3 +177,28 @@ const opposite = (n) => n * -1
 //Your classmates asked you to copy some paperwork for them.You know that there are 'n' classmates and the paperwork has 'm' pages.
 //Your task is to calculate how many blank pages do you need.If n < 0 or m < 0 return 0.
 const paperwork = (n, m) => n > 0 && m > 0 ? n * m : 0
+
+/*
+Create a function finalGrade, which calculates the final grade of a student depending on two parameters: a grade for the exam and a number of completed projects.
+This function should take two arguments: exam - grade for exam (from 0 to 100); projects - number of completed projects (from 0 and above);
+This function should return a number (final grade). There are four types of final grades:
+
+100, if a grade for the exam is more than 90 or if a number of completed projects more than 10.
+90, if a grade for the exam is more than 75 and if a number of completed projects is minimum 5.
+75, if a grade for the exam is more than 50 and if a number of completed projects is minimum 2.
+0, in other cases
+ */
+
+function finalGrade(exam, projects) {
+   if (exam > 90 || projects > 10) {
+      return 100
+   }
+   else if (exam > 75 && projects >= 5) {
+      return 90
+   } else if (exam > 50 && projects >= 2) {
+      return 75
+   }
+   return 0
+}
+
+//=======================================================================//
