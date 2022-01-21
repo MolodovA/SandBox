e782937c0a685658c393beebe0403621ba0cc776
MolodovA <80354289+MolodovA@users.noreply.github.com>
2022-01-21 22:43:03 +0300

    solution task codewars

--- js/script.js
+++ js/script.js
@@ -413,7 +413,16 @@ function squareSum(numbers) {
 squareSum([0, 3, 4, 5])
 
 
-//function basicOp(operation, value1, value2)
-////{
-  // Code
-//}
+function basicOp(operation, value1, value2) {
+   switch (operation) {
+      case '+':
+         return value1 + value2
+      case '-':
+         return value1 - value2
+      case '*':
+         return value1 * value2
+      case '/':
+         return value1 / value2
+   }
+}
+console.log(basicOp('-', 4, 7))
